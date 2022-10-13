6b178f4a62fc9f9b7c5d92e6112a0b0386c05c9d
FerCodeHerrera <luisherreratrabajos1998@gmail.com>
2022-10-13 08:18:00 -0500

    Se subieron los archivos del proyecto a GitHub

---
+++ app/src/main/java/com/android/example/jetpackcomposeinstagram/login/LoginViewModel.kt
@@ -0,0 +1,29 @@
+package com.android.example.jetpackcomposeinstagram.login
+
+import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel :ViewModel(){
+
+    private  val _email = MutableLiveData<String>()
+    val email: LiveData<String> = _email
+
+    private val _password = MutableLiveData<String>()
+    val password : LiveData<String> = _password
+
+    private val _isLoginEnable = MutableLiveData<Boolean>()
+    val isLoginEnable : LiveData<Boolean> = _isLoginEnable
+
+    fun onLoginChanged(email: String, password: String){
+        _email.value = email
+        _password.value = password
+
+        enableLogin(email,password)
+    }
+
+    fun enableLogin(email: String,password: String){
+        _isLoginEnable.value  = (Patterns.EMAIL_ADDRESS.matcher(email).matches() &&  password.length > 6)
+    }
+}
